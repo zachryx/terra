944f2130bd4d0da57b66aac342fa54ee0626d8e9
Omolade Sunday <omoladesunday2017@gmail.com>
2026-03-01 02:57:13 +0100

    feat: update app dependencies, revamp prisma setup

--- src/infrastructure/prisma/prisma.service.ts
+++ src/infrastructure/prisma/prisma.service.ts
@@ -1,8 +1,16 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL as string,
+    });
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
